c250bb2bded94632682677f9455ea977c6268242
Sankalp <sankalppawar619@gmail.com>
2024-10-19 15:58:22 +0530

    new

---
+++ src/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express();
+
+app.use("/home", (req,res)=>{
+    res.send("Home")
+});
+
+app.use("/test", (req,res)=>{
+    res.send("test")
+});
+
+app.use("/", (req,res)=>{
+    res.send("H")
+});
+
+app.listen(7777, ()=> {
+    console.log("Server is running on port 7777")
+})
